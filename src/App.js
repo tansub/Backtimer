8cb6ef7669546d401bd78c85081f6e411d1cf268
tansub <tansubaktybaeva@gmail.com>
2024-12-27 08:15:13 -0500

    add to remote github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CountdownTimer from './components/Timer';
+import { getRemainingTimeStampMs} from './components/Utils'
+
+function App() {
+  return (
+      <div className='container'> 
+        <h1>BIG SALE ON LIFETIME PLAN</h1>
+        <h4>there is very little left</h4>
+        <CountdownTimer countdownTimestampMs={1800000000000}/>
+      </div>
+  );
+}
+
+export default App;
